9fb1895065869330714d6c6dc07f53eb3771a418
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-08-14 15:01:02 +0300

    Update class-unknown-cookie-adapter-factory.php

--- features/models/unknowns/class-unknown-cookie-adapter-factory.php
+++ features/models/unknowns/class-unknown-cookie-adapter-factory.php
@@ -18,35 +18,35 @@ final class Unknown_Cookie_Adapter_Factory implements Cookie_Adapter_Factory
 {
 	public function __construct(
 		private string $current_language,
-		Cookie_Category_Factory $categories,
-		Cookie_Type_Factory $types,
+		private Cookie_Category_Factory $categories,
+		private Cookie_Type_Factory $types,
 	) {}
 
 	public function create_known_cookie( Known_Cookie_Data $data ): Cookie_Adapter
 	{
-		return new Default_Cookie_Adapter( ...array(
+		return new Unknown_Cookie_Adapter( ...array(
 			'current_language' => $this->current_language,
 			'issuer' => $data->issuer(),
 			'name' => $data->name(),
 			'label' => $data->label(),
 			'descriptions' => $data->descriptionTranslations(),
 			'retentions' => $data->retentionTranslations(),
-			'type' => $types->from_string( $data->type() ),
-			'category' => $categories->from_string( $data->category() ),
+			'type' => $this->types->from_string( $data->type() ),
+			'category' => $this->categories->from_string( $data->category() ),
 		) );
 	}
 
 	public function create_adapter( mixed $data ): Cookie_Adapter
 	{
-		return new Default_Cookie_Adapter( ...array(
+		return new Unknown_Cookie_Adapter( ...array(
 			'current_language' => $this->current_language,
 			'issuer' => '',
 			'name' => 'unknown',
 			'label' => __( 'Unknown', 'wordpress-helfi-cookie-consent' ),
 			'descriptions' => array(),
 			'retentions' => array(),
-			'type' => $types->from_string( 'unknown' ),
-			'category' => $categories->from_string( 'unknown' ),
+			'type' => $this->types->from_string( 'unknown' ),
+			'category' => $this->categories->from_string( 'unknown' ),
 		) );
 	}
 }
