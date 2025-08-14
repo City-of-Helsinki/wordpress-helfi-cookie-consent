c43e5f4c736d753553033e2e8def0063531536f8
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-08-14 15:25:26 +0300

    Update providers.php

--- features/models/providers.php
+++ features/models/providers.php
@@ -19,7 +19,44 @@ use CityOfHelsinki\WordPress\CookieConsent\Features\Models\Unknowns\Unknown_Cook
 
 \add_action( 'wordpress_helfi_cookie_consent_loaded', __NAMESPACE__ . '\\init', 9 );
 function init(): void {
-	$repository = default_cookie_repository();
+	$current_language = \apply_filters(
+		'wordpress_helfi_cookie_consent_current_language',
+		'en'
+	);
+
+	$types = \apply_filters(
+		'wordpress_helfi_cookie_consent_cookie_type_factory',
+		create_cookie_type_factory( $current_language )
+	);
+
+	$categories = \apply_filters(
+		'wordpress_helfi_cookie_consent_cookie_category_factory',
+		create_cookie_category_factory( $current_language )
+	);
+
+	$repository = create_cookie_repository( array(
+		'database' => \apply_filters(
+			'wordpress_helfi_cookie_consent_cookie_database',
+			create_unknown_cookie_database()
+		),
+		'factory' => \apply_filters(
+			'wordpress_helfi_cookie_consent_cookie_adapter_factory',
+			create_unknown_cookie_adapter_factory( array(
+				'current_language' => $current_language,
+				'categories' => $categories,
+				'types' => $types,
+			) ),
+			$categories,
+			$types,
+			$current_language
+		),
+		'known_cookies' => known_cookies_data(
+			\apply_filters(
+				'wordpress_helfi_cookie_consent_known_cookies',
+				array()
+			)
+		),
+	) );
 
 	\add_filter(
 		'wordpress_helfi_cookie_consent_cookie_repository',
@@ -29,103 +66,26 @@ function init(): void {
 	);
 }
 
-function default_cookie_repository(): Cookie_Repository {
-	return create_cookie_repository( array(
-		'database' => create_cookie_database(),
-		'factory' => create_cookie_adapter_factory(
-			create_cookie_category_factory(),
-			create_cookie_type_factory()
-		),
-		'known_cookies' => known_cookies_data(),
-	) );
-}
-
 function create_cookie_repository( array $config ): Cookie_Repository {
 	return new Cookie_Repository( ...$config );
 }
 
-function create_cookie_database(): Cookie_Database {
-	static $default;
-
-	$database = \apply_filters(
-		'wordpress_helfi_cookie_consent_cookie_database',
-		null
-	);
-
-	if ( $database ) {
-		return $database;
-	}
-
-	if ( empty( $default ) ) {
-		$default = new Unknown_Cookie_Database();
-	}
-
-	return $default;
+function create_unknown_cookie_database(): Cookie_Database {
+	return new Unknown_Cookie_Database();
 }
 
-function create_cookie_adapter_factory( Category_Factory $categories, Type_Factory $types ): Cookie_Adapter_Factory {
-	static $default;
-
-	$factory = \apply_filters(
-		'wordpress_helfi_cookie_consent_cookie_adapter_factory',
-		null,
-		$categories,
-		$types
-	);
-
-	if ( $factory ) {
-		return $factory;
-	}
-
-	if ( empty( $default ) ) {
-		$default = create_unknown_cookie_adapter_factory( $categories, $types );
-	}
-
-	return $default;
+function create_unknown_cookie_adapter_factory( array $config ): Cookie_Adapter_Factory {
+	return new Unknown_Cookie_Adapter_Factory( ...$config );
 }
 
-function create_unknown_cookie_adapter_factory( Category_Factory $categories, Type_Factory $types ): Cookie_Adapter_Factory {
-	return new Unknown_Cookie_Adapter_Factory(
-		\apply_filters(
-			'wordpress_helfi_cookie_consent_current_language',
-			'en'
-		),
-		$categories,
-		$types
-	);
+function create_cookie_category_factory( string $current_language ): Category_Factory {
+	return new Cookie_Category_Factory( $current_language, new Cache() );
 }
 
-function create_cookie_category_factory(): Category_Factory {
-	return \apply_filters(
-		'wordpress_helfi_cookie_consent_cookie_category_factory',
-		new Cookie_Category_Factory(
-			\apply_filters(
-				'wordpress_helfi_cookie_consent_current_language',
-				'en'
-			),
-			new Cache()
-		)
-	);
+function create_cookie_type_factory( string $current_language ): Type_Factory {
+	return new Cookie_Type_Factory( $current_language, new Cache() );
 }
 
-function create_cookie_type_factory(): Type_Factory {
-	return \apply_filters(
-		'wordpress_helfi_cookie_consent_cookie_type_factory',
-		new Cookie_Type_Factory(
-			\apply_filters(
-				'wordpress_helfi_cookie_consent_current_language',
-				'en'
-			),
-			new Cache()
-		)
-	);
-}
-
-function known_cookies_data(): Known_Cookies {
-	return new Known_Cookies(
-		\apply_filters(
-			'wordpress_helfi_cookie_consent_known_cookies',
-			array()
-		)
-	);
+function known_cookies_data( array $cookies ): Known_Cookies {
+	return new Known_Cookies( $cookies );
 }
