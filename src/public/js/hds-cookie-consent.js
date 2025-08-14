882d644181262078fbfa2d3de882ae63116d39c3
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-08-14 13:38:02 +0300

    Update hds-cookie-consent.js

--- src/public/js/hds-cookie-consent.js
+++ src/public/js/hds-cookie-consent.js
@@ -2,7 +2,7 @@
   if (hds.CookieConsentCore && config.routes) {
     init_handler(config);
 
-    function init_handler({routes, currentLanguage, cookiesHandler}) {
+    function init_handler({routes, options, cookiesHandler}) {
       let handlers = {
         complianz: complianz_cookies_handler,
         none: no_cookies_handler,
@@ -13,7 +13,7 @@
       }
 
       handlers[cookiesHandler](
-        create_cookie_consent(routes.settings, currentLanguage)
+        create_cookie_consent(routes.settings, options)
       );
     }
 
@@ -22,49 +22,32 @@
       const deny = (group) => cmplz_set_consent(group, 'deny');
       const denyAll = () => cmplz_deny_all();
 
-      window.addEventListener('hds-cookie-consent-ready', event => {
+      const handleConsentChanges = () => {
         if (hds.cookieConsent) {
           hds.cookieConsent.getAllConsentStatuses()
             .forEach(({consented, group}) => consented ? allow(group) : deny(group));
         }
-      });
+      };
 
-      // window.addEventListener('hds-cookie-consent-unapproved-item-found', event => {
-      //   console.log(event);
-      // });
+      window.addEventListener('hds-cookie-consent-ready', handleConsentChanges);
+      window.addEventListener('hds-cookie-consent-changed', handleConsentChanges);
 
-      window.addEventListener('hds-cookie-consent-changed', event => {
-        const {acceptedGroups} = event.detail || [];
-
-        if (acceptedGroups.length > 0) {
-          event.detail.acceptedGroups.forEach(group => allow(group));
-        } else {
-          denyAll();
-        }
-      });
+      // window.addEventListener('hds-cookie-consent-unapproved-item-found', event => console.log(event));
     }
 
     function no_cookies_handler() {
-      console.log('no_cookies_handler');
+      console.error('no cookies handler');
     }
 
-    function create_cookie_consent(settingsUrl, language) {
+    function create_cookie_consent(settingsUrl, options) {
       return hds.CookieConsentCore.create(
-        (() => {
+        (({language}) => {
           let params = new URLSearchParams();
           params.append('lang', language);
 
           return settingsUrl + '?' + params.toString();
-        })(),
-        {
-          // focusTargetSelector: '',
-          language: language,
-          // pageContentSelector: '',
-          // settingsPageSelector: '',
-          // spacerParentSelector: '',
-          submitEvent: true,
-          // targetSelector: true,
-        }
+        })(options),
+        options
       );
     }
   }
