8a193f26b0c825ac4bee7f51ec956215d7239ef4
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-08-14 14:57:26 +0300

    Update class-creates-cookie-groups.php

--- features/rest/settings/traits/class-creates-cookie-groups.php
+++ features/rest/settings/traits/class-creates-cookie-groups.php
@@ -47,7 +47,7 @@ trait Creates_Cookie_Groups
 	protected function format_cookie( Cookie_Adapter $cookie ): array
 	{
 		return array(
-			'name' => $cookie->name(),
+			'name' => $cookie->label(),
 			'host' => $cookie->issuer(),
 			'description' => array(
 				'fi' => $cookie->description( 'fi' ),
