16302a39bb120abaa4e4dc4d2850ec1a7b8e485a
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-08-14 14:51:34 +0300

    Update providers.php

--- features/models/providers.php
+++ features/models/providers.php
@@ -30,17 +30,18 @@ function init(): void {
 }
 
 function default_cookie_repository(): Cookie_Repository {
-	return create_cookie_repository(
-		create_cookie_database(),
-		create_cookie_adapter_factory(
+	return create_cookie_repository( array(
+		'database' => create_cookie_database(),
+		'factory' => create_cookie_adapter_factory(
 			create_cookie_category_factory(),
 			create_cookie_type_factory()
-		)
-	);
+		),
+		'known_cookies' => known_cookies_data(),
+	) );
 }
 
-function create_cookie_repository( Cookie_Database $database, Cookie_Adapter_Factory $factory ): Cookie_Repository {
-	return new Cookie_Repository( $database, $factory );
+function create_cookie_repository( array $config ): Cookie_Repository {
+	return new Cookie_Repository( ...$config );
 }
 
 function create_cookie_database(): Cookie_Database {
@@ -77,38 +78,54 @@ function create_cookie_adapter_factory( Category_Factory $categories, Type_Facto
 	}
 
 	if ( empty( $default ) ) {
-		$default = new Unknown_Cookie_Adapter_Factory( $categories, $types );
+		$default = create_unknown_cookie_adapter_factory( $categories, $types );
 	}
 
 	return $default;
 }
 
-function create_cookie_category_factory(): Category_Factory {
-	static $factory = new Cookie_Category_Factory(
+function create_unknown_cookie_adapter_factory( Category_Factory $categories, Type_Factory $types ): Cookie_Adapter_Factory {
+	return new Unknown_Cookie_Adapter_Factory(
 		\apply_filters(
 			'wordpress_helfi_cookie_consent_current_language',
 			'en'
 		),
-		new Cache()
+		$categories,
+		$types
 	);
+}
 
+function create_cookie_category_factory(): Category_Factory {
 	return \apply_filters(
 		'wordpress_helfi_cookie_consent_cookie_category_factory',
-		$factory
+		new Cookie_Category_Factory(
+			\apply_filters(
+				'wordpress_helfi_cookie_consent_current_language',
+				'en'
+			),
+			new Cache()
+		)
 	);
 }
 
 function create_cookie_type_factory(): Type_Factory {
-	static $factory = new Cookie_Type_Factory(
-		\apply_filters(
-			'wordpress_helfi_cookie_consent_current_language',
-			'en'
-		),
-		new Cache()
-	);
-
 	return \apply_filters(
 		'wordpress_helfi_cookie_consent_cookie_type_factory',
-		$factory
+		new Cookie_Type_Factory(
+			\apply_filters(
+				'wordpress_helfi_cookie_consent_current_language',
+				'en'
+			),
+			new Cache()
+		)
+	);
+}
+
+function known_cookies_data(): Known_Cookies {
+	return new Known_Cookies(
+		\apply_filters(
+			'wordpress_helfi_cookie_consent_known_cookies',
+			array()
+		)
 	);
 }
