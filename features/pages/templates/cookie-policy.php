23811a04361ceb35f3c04f4339543be197e02ebf
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-08-26 16:13:41 +0300

    Create cookie-policy.php

---
+++ features/pages/templates/cookie-policy.php
@@ -0,0 +1,15 @@
+<?php
+
+get_header();
+
+do_action( 'wordpress_helfi_cookie_consent_page_before' );
+
+do_action( 'wordpress_helfi_cookie_consent_page_top' );
+
+do_action( 'wordpress_helfi_cookie_consent_page' );
+
+do_action( 'wordpress_helfi_cookie_consent_page_bottom' );
+
+do_action( 'wordpress_helfi_cookie_consent_page_after' );
+
+get_footer();
