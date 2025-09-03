d66ccfcfe8e7750bbbb795daac9bc11380ac380b
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-09-03 10:53:38 +0300

    Update wordpress-helfi-cookie-consent.php

--- wordpress-helfi-cookie-consent.php
+++ wordpress-helfi-cookie-consent.php
@@ -24,38 +24,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
   * Setup
   */
-require_once plugin_dir_path( __FILE__ ) . 'constants.php';
+require_once \plugin_dir_path( __FILE__ ) . 'constants.php';
 define_constants( __FILE__ );
 
-require_once plugin_dir_path( __FILE__ ) . 'functions.php';
+require_once \plugin_dir_path( __FILE__ ) . 'functions.php';
 load_includes();
 
 spl_autoload_register( __NAMESPACE__ . '\\class_loader' );
 
-add_action( 'wordpress_helfi_cookie_consent_setup', __NAMESPACE__ . '\\setup_filters', 0 );
-add_action( 'wordpress_helfi_cookie_consent_setup', __NAMESPACE__ . '\\load_features', 1 );
-add_action( 'wordpress_helfi_cookie_consent_setup', __NAMESPACE__ . '\\load_integrations', 1 );
+\add_action( 'wordpress_helfi_cookie_consent_setup', __NAMESPACE__ . '\\setup_filters', 0 );
+\add_action( 'wordpress_helfi_cookie_consent_setup', __NAMESPACE__ . '\\load_features', 1 );
+\add_action( 'wordpress_helfi_cookie_consent_setup', __NAMESPACE__ . '\\load_integrations', 1 );
 
-add_action( 'plugins_loaded', __NAMESPACE__ . '\\setup', 1 );
-add_action( 'plugins_loaded', __NAMESPACE__ . '\\loaded', 10 );
+\add_action( 'plugins_loaded', __NAMESPACE__ . '\\setup', 1 );
+\add_action( 'plugins_loaded', __NAMESPACE__ . '\\loaded', 10 );
 
 /**
   * Init
   */
-add_action( 'init', __NAMESPACE__ . '\\textdomain' );
-add_action( 'init', __NAMESPACE__ . '\\init', 100 );
+\add_action( 'init', __NAMESPACE__ . '\\textdomain' );
+\add_action( 'init', __NAMESPACE__ . '\\init', 100 );
 
 /**
   * Activation
   */
-// register_activation_hook( __FILE__, __NAMESPACE__ . '\\activate' );
+\register_activation_hook( __FILE__, __NAMESPACE__ . '\\activate' );
 
 /**
   * Deactivation
   */
-// register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate' );
+\register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate' );
 
 /**
   * Uninstall
   */
-// register_uninstall_hook( __FILE__, __NAMESPACE__ . '\\uninstall' );
+// \register_uninstall_hook( __FILE__, __NAMESPACE__ . '\\uninstall' );
