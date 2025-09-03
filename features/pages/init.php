938a5cac8a03dc0ce647e52c94b0699aea187f79
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-09-03 09:20:03 +0300

    Update init.php

--- features/pages/init.php
+++ features/pages/init.php
@@ -76,40 +76,12 @@ function document_pages_metabox(): void {
 }
 
 function render_document_pages_metabox(): void {
-	$pages = \apply_filters( 'wordpress_helfi_cookie_consent_nav_menu_metabox_pages', array() );
-	?>
-	<div id="posttype-helfi-cookie-consent-page" class="posttypediv">
-        <div id="tabs-panel-helfi-cookie-consent-page" class="tabs-panel tabs-panel-active">
-            <ul id="helfi-cookie-consent-page-checklist" class="categorychecklist form-no-clear">
-				<?php
-					foreach( $pages as $page ) {
-						printf(
-							'<li>
-			                    <label class="menu-item-title">
-			                        <input type="checkbox" class="menu-item-checkbox" name="menu-item[-1][menu-item-object-id]" value="-1"> %1$s
-			                    </label>
-			                    <input type="hidden" class="menu-item-type" name="menu-item[-1][menu-item-type]" value="%2$s">
-			                    <input type="hidden" class="menu-item-title" name="menu-item[-1][menu-item-title]" value="%1$s">
-			                    <input type="hidden" class="menu-item-url" name="menu-item[-1][menu-item-url]" value="#">
-			                </li>',
-							\esc_html( $page->title() ),
-							\esc_attr( $page->type() )
-						);
-					}
-				?>
-            </ul>
-        </div>
-        <p class="button-controls">
-            <span class="add-to-menu">
-                <input
-					id="submit-posttype-helfi-cookie-consent-page"
-					class="button-secondary submit-add-to-menu right"
-					type="submit"
-					name="add-post-type-menu-item"
-					value="<?php \esc_attr_e( 'Add to Menu' ); ?>">
-                <span class="spinner"></span>
-            </span>
-        </p>
-    </div>
-	<?php
+	$template_path = \apply_filters(
+		'wordpress_helfi_cookie_consent_path_to_php_file',
+		array( 'features', 'pages', 'templates', 'document-pages-metabox' )
+	);
+
+	if ( is_string( $template_path ) && $template_path ) {
+		require_once $template_path;
+	}
 }
