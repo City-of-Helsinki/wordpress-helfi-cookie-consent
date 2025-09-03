a1989bbad3d6d67991b6d6e0a404727d35d4d3c4
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-09-03 10:26:47 +0300

    Update init.php

--- features/notices/init.php
+++ features/notices/init.php
@@ -10,17 +10,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 \add_action( 'admin_init', __NAMESPACE__ . '\\init' );
 function init(): void {
-	\add_action( 'admin_notices', __NAMESPACE__ . '\\display_cookie_provider_status' );
+	$hooks = create_admin_notices_hooks();
+
+	\add_action(
+		'wordpress_helfi_cookie_consent_add_admin_notice',
+		array( $hooks, 'add_notice' )
+	);
+
+	\add_action(
+		'admin_notices',
+		array( $hooks, 'display_notices' )
+	);
 }
 
-function display_cookie_provider_status(): void {
-	if ( ! \apply_filters( 'wordpress_helfi_cookie_consent_has_cookie_provider', false ) ) {
-		$notice = new Missing_Cookie_Provider_Notice();
-
-		printf(
-			'<div class="notice notice-%s">%s</div>',
-			\esc_attr( $notice->type() ),
-			\wp_kses_post( $notice->message() )
-		);
-	}
+function create_admin_notices_hooks(): Admin_Notices_Hooks {
+	return new Admin_Notices_Hooks();
 }
