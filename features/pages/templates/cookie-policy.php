b1217067c9191669c1ae54fb3ac46756b3f42049
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-10-06 15:12:33 +0300

    Update cookie-policy.php

--- features/pages/templates/cookie-policy.php
+++ features/pages/templates/cookie-policy.php
@@ -1,15 +1,17 @@
 <?php
 
+$current_page = apply_filters( 'wordpress_helfi_cookie_consent_current_page', null );
+
 get_header();
 
-do_action( 'wordpress_helfi_cookie_consent_page_before' );
+do_action( 'wordpress_helfi_cookie_consent_page_before', $current_page );
 
-do_action( 'wordpress_helfi_cookie_consent_page_top' );
+do_action( 'wordpress_helfi_cookie_consent_page_top', $current_page );
 
-do_action( 'wordpress_helfi_cookie_consent_page' );
+do_action( 'wordpress_helfi_cookie_consent_page', $current_page );
 
-do_action( 'wordpress_helfi_cookie_consent_page_bottom' );
+do_action( 'wordpress_helfi_cookie_consent_page_bottom', $current_page );
 
-do_action( 'wordpress_helfi_cookie_consent_page_after' );
+do_action( 'wordpress_helfi_cookie_consent_page_after', $current_page );
 
 get_footer();
