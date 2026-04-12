39688c13cb1484d98281c751225ddeaf737b8cca
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-12 22:49:14 +0300

    Update wordpress-helfi-cookie-consent.php

--- wordpress-helfi-cookie-consent.php
+++ wordpress-helfi-cookie-consent.php
@@ -4,7 +4,7 @@
 	* Description: WordPress Implementation of HDS CookieConsent
 	* Requires at least: 6.7
 	* Requires PHP: 8.2
-	* Version: 3.0.2
+	* Version: 3.1.0
 	* Author: City of Helsinki
 	* Author URI: https://www.hel.fi
 	* License: GPL v2 or later
