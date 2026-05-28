e807a49851de1d261dbf8f59af4c0bc83c5173d1
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-05-28 17:48:19 +0300

    include default wp cookie definitions

--- features/models/class-known-cookies.php
+++ features/models/class-known-cookies.php
@@ -10,6 +10,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use CityOfHelsinki\WordPress\CookieConsent\Features\Interfaces\Known_Cookie_Data;
 use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\HDS_Cookie_Consent;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\Comment_Author_Email;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\Comment_Author_Url;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\Comment_Author;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WordPress_Logged_In;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WordPress_Sec;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WordPress_Test_Cookie;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Api_Schema_Model;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Autosave;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Data_User;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Lang;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Postpass;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Preferences_User;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Saving_Post;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Settings_Time;
+use CityOfHelsinki\WordPress\CookieConsent\Features\Cookies\WordPress\WP_Settings;
 use Closure;
 
 final class Known_Cookies
@@ -49,8 +64,38 @@ final class Known_Cookies
 
 	private function default_cookies(): array
 	{
-		return array(
+		$cookies = array(
 			HDS_Cookie_Consent::class,
+			WordPress_Test_Cookie::class,
+			WP_Postpass::class,
+			WP_Lang::class,
 		);
+
+		if ( \is_user_logged_in() ) {
+			$cookies[] = WordPress_Logged_In::class;
+			$cookies[] = WordPress_Sec::class;
+			$cookies[] = WP_Api_Schema_Model::class;
+			$cookies[] = WP_Data_User::class;
+			$cookies[] = WP_Preferences_User::class;
+			$cookies[] = WP_Settings_Time::class;
+			$cookies[] = WP_Settings::class;
+
+			if (
+				\current_user_can( 'edit_posts' )
+				|| \current_user_can( 'edit_pages' )
+			) {
+				$cookies[] = WP_Autosave::class;
+				$cookies[] = WP_Saving_Post::class;
+			}
+
+		}
+
+		if ( \apply_filters( 'wordpress_helfi_cookie_consent_comments_enabled', false ) ) {
+			$cookies[] = Comment_Author_Email::class;
+			$cookies[] = Comment_Author_Url::class;
+			$cookies[] = Comment_Author::class;
+		}
+
+		return $cookies;
 	}
 }
